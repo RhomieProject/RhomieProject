a2fa1d1568e29f5d4eac6f67a2e0ec404e109ba2
GiladMoalem <giladmoa@gmail.com>
2022-01-06 19:03:10 +0200

    Image Profile added
    in "my account" page added option to add profile image and change.
    
    based on mvc design and useed in Firebase Storage service.

--- Rhomie/app/src/main/java/com/example/rhomie/Models/SettingModel.java
+++ Rhomie/app/src/main/java/com/example/rhomie/Models/SettingModel.java
@@ -1,9 +1,13 @@
 package com.example.rhomie.Models;
 
+import android.net.Uri;
+
 import androidx.annotation.NonNull;
 
 import com.example.rhomie.Objects.IUser;
 import com.example.rhomie.Objects.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -11,6 +15,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.Observable;
 
@@ -22,11 +29,16 @@ public class SettingModel extends Observable {
 
     private String details;
 
+//    ______
+    private StorageReference storageReference;
+
 
     public SettingModel(){
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         users = db.getReference("Users");
         user = FirebaseAuth.getInstance().getCurrentUser();
+
+        storageReference = FirebaseStorage.getInstance().getReference(user.getUid().toString());
     }
 
     public void getDetails() {
@@ -53,6 +65,32 @@ public class SettingModel extends Observable {
 
     public void Logout() {
         FirebaseAuth.getInstance().signOut();
+    }
+
+
+    public void setProfileImage(Uri imageUri){
+        storageReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                Picasso.get().load(uri).into(profile_image);
+                //uploade the controller
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                setChanged();
+                notifyObservers(e);
+            }
+        });
+    }
+    public void getProfileImage(){
+        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                setChanged();
+                notifyObservers(uri);
+            }
+        });
     }
 
 }
--- Rhomie/app/src/main/java/com/example/rhomie/View/SettingActivity.java
+++ Rhomie/app/src/main/java/com/example/rhomie/View/SettingActivity.java
@@ -1,13 +1,19 @@
 package com.example.rhomie.View;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.rhomie.Controller.MyAccountController;
+import com.squareup.picasso.Picasso;
 import com.example.rhomie.Controller.SettingController;
 import com.example.rhomie.R;
 
@@ -15,6 +21,8 @@ public class SettingActivity extends AppCompatActivity {
     private TextView greetingTextView,emailTextView,fullNameTextView, phoneNumberTextView, idTextView;
     private String email,firstName,lastName,phoneNumber,id;
     private SettingController controller;
+
+    private ImageView profileImage, setImageIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +38,56 @@ public class SettingActivity extends AppCompatActivity {
 
         controller.getDetails();
 
+//_____________________________________________________________________//
+//                  Image profile
 
+        profileImage = findViewById(R.id.profileImage);
+        setImageIcon = findViewById(R.id.setImageView);
+
+        setImageIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open gallery
+                Intent openGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGallery, 1000);
+            }
+        });
+
+        controller.getProfileImage();
+
+//_____________________________________________________________________//
+    }
+
+
+
+    //for profile image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000){
+            if (resultCode == Activity.RESULT_OK) {
+                Uri imageUri = data.getData();
+                profileImage.setImageURI(imageUri);
+
+                //upload the image in firebase
+                controller.setProfileImage(imageUri);
+            }
+        }
 
     }
+
+    public void setProfileImage(Uri image){
+        Picasso.get().load(image).into(profileImage);
+    }
+
+    public void OnSetProfileImageError(String message){
+        Toast.makeText(SettingActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
 
     public void getDetails(String greeting,String email,String firstName,String lastName,String phoneNumber,String id){
         this.greetingTextView.setText(greeting);
